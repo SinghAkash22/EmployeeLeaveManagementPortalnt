da39d7a2d5cb0cc63b4396958ab8dd09810c06be
Akash AmanSingh <67159568+SinghAkash22@users.noreply.github.com>
2024-10-08 00:54:13 +0530

    Delete EmplyeeLeaveManagementPortal directory

--- EmplyeeLeaveManagementPortal/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EmplyeeLeaveManagementPortal.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
